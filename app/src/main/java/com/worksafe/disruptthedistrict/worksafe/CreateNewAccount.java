619ce9f1efd9931d6f8404e12128e95e72ba5ec0
Adam Morrissett <22103567+morrissettal2@users.noreply.github.com>
2018-03-04 03:14:58 -0500

    Added back button for registration Activity.

--- app/src/main/java/com/worksafe/disruptthedistrict/worksafe/CreateNewAccount.java
+++ app/src/main/java/com/worksafe/disruptthedistrict/worksafe/CreateNewAccount.java
@@ -1,8 +1,11 @@
 package com.worksafe.disruptthedistrict.worksafe;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -14,6 +17,15 @@ public class CreateNewAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_account);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.colorWhite),
+                PorterDuff.Mode.SRC_ATOP);
+
         createNewAccount = (Button)findViewById(R.id.newAccountButton);
         createNewAccount.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -42,5 +54,17 @@ public class CreateNewAccount extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
